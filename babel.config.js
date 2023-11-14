9f1a9815a5970d511a9e1d5c3d87bf1ff5a5f9b9
mathieu enault <me94110@gmail.com>
2023-11-15 00:03:01 +0100

    adding missing config files information from previous commit

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,25 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
+      "expo-router/babel",
+      ["@babel/plugin-transform-flow-strip-types"],
+      ["@babel/plugin-proposal-decorators", { legacy: true }],
+      ["@babel/plugin-proposal-class-properties", { loose: true }],
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@Components": "./src/components",
+            "@Screens": "./src/screens",
+            "@Redux": "./src/redux",
+            "@Assets": "./src/assets",
+          },
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+      ],
     ],
   };
 };
